d648b5024625133373f02bb2722ab69e3fcc0a48
itosun <ismailtosuntnyl@gmail.com>
2022-10-30 21:47:54 +0000

    initial

---
+++ test.py
@@ -0,0 +1,32 @@
+from simpleai.search.viewers import WebViewer
+
+from simpleai.search import SearchProblem, astar
+my_viewer = WebViewer()
+GOAL = 'HELLO WORLD'
+
+
+class HelloProblem(SearchProblem):
+    def actions(self, state):
+        if len(state) < len(GOAL):
+            return list(' ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+        else:
+            return []
+
+    def result(self, state, action):
+        return state + action
+
+    def is_goal(self, state):
+        return state == GOAL
+
+    def heuristic(self, state):
+        # how far are we from the goal?
+        wrong = sum([1 if state[i] != GOAL[i] else 0
+                    for i in range(len(state))])
+        missing = len(GOAL) - len(state)
+        return wrong + missing
+
+problem = HelloProblem(initial_state='')
+result = astar(problem,viewer=my_viewer,graph_search=True)
+
+print(result.state)
+print(result.path())
